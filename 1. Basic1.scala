0f961213b382bfab99d699181458f5cef0151d90
NEU-ZYXi <stephenxi@hotmail.com>
2019-05-04 16:53:59 -0700

    Update 1. Basic1.scala

--- 1. Basic1.scala
+++ 1. Basic1.scala
@@ -37,3 +37,80 @@ def timesTwo(i: Int): Int = {  // use {} for multiple lines functions
   println("hello world")
   i * 2
 }
+
+// underscore as an unnamed magical wildcard
+def add(m: Int, n: Int): Int = m + n
+val add2 = add(2, _: Int)
+add2(3)
+res0: Int = 5
+
+// curried functions
+def multiply(m: Int)(n: Int): Int = m * n
+multiply(2)(3)
+res0: Int = 6
+
+val timesTwo = multiply(2) _
+timesTwo: (Int) => Int = <funciton1>
+timesTwo(3)
+res1: Int = 6
+
+// variable length arguments
+def capitalizeAll(args: String*) = {
+  args.map {
+    arg => arg.capitalize 
+  }
+}
+
+// Classes
+class Calculator {
+  val brand: String = "a"
+  def add(m: Int, n: Int): Int = m + n
+}
+
+val calc = new Calculator
+calc.add(1, 2)
+res0: Int = 3
+calc.brand
+res1: String = "a"
+
+// constructor
+calss Calculator(brand: String) {
+  val color: String = if (brand == "a") {
+    "black"
+  } else {
+    "white"
+  }
+}
+
+val calc = new Calculator("a")
+
+// Inheritance
+extends
+
+// abstract classes
+abstract class Shape {
+  def getArea(): Int 
+}
+
+class Circle(r: Int) extends Shape {
+  def getArea(): Int = {r * r * 3} 
+}
+
+// traits are collections of fields and behaviors that can extend or mixin to classes, one class can extend several traits using 'with'
+trait Car {
+  val brand: String 
+}
+
+trait Shiny {
+  val factor: Int
+}
+
+class BMW extends Car {
+  val brand = "BMW" 
+}
+
+class BMW extends Car with Shiny {
+  val brand = "BMW"
+  val factor = 10
+}
+
